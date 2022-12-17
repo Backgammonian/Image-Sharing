eb855b69fca91473e71b84c634d66944b433e77f
Dexp <Dexp@DESKTOP-G7BK324>
2022-12-18 04:01:38 +0900

    added ip geolocation

---
+++ MyWebApp/Data/IPLocationHelper.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace MyWebApp.Data
+{
+    public static class IPLocationHelper
+    {
+        private const string _unknownIP = "Unknown IP";
+
+        public static async Task<string> GetLocation(HttpContext context)
+        {
+            try
+            {
+                var remoteIpAddress = context.Connection.RemoteIpAddress;
+                Debug.WriteLine($"(IPLocationHelper_GetLocation_remoteIpAddress) {remoteIpAddress}");
+
+                var url = $"http://ip-api.com/json/{remoteIpAddress}?fields=66846719";
+                using var client = new HttpClient();
+                var response = await client.GetAsync(url);
+                var content = await response.Content.ReadAsStringAsync();
+                var ipInfo = JsonConvert.DeserializeObject<IpApiResponse>(content);
+                
+                if (ipInfo != null &&
+                    ipInfo.Status == "success")
+                {
+                    return $"{ipInfo.Country}, {ipInfo.City}";
+                }
+
+                return $"{_unknownIP} ({remoteIpAddress})";
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"(IPLocationHelper_GetLocation) {ex}");
+                return _unknownIP;
+            }
+        }
+    }
+}

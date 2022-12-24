132084b89e8aeb8012b03f858c4bfc5142748720
Dexp <Dexp@DESKTOP-G7BK324>
2022-12-24 11:15:54 +0900

    clean up

--- MyWebApp/ViewModels/DashboardViewModel.cs
+++ MyWebApp/ViewModels/DashboardViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace MyWebApp.ViewModels
+﻿using MyWebApp.Models;
+
+namespace MyWebApp.ViewModels
 {
     public sealed class DashboardViewModel
     {
         public IEnumerable<NoteDetailsViewModel> UserNotes { get; set; } = Enumerable.Empty<NoteDetailsViewModel>();
+        public UserModel User { get; set; } = new UserModel();
+        public UserImageModel ProfilePicture { get; set; } = new UserImageModel();
     }
 }
